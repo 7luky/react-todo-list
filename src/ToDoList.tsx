2d0ee7bfcbf6a9f78b60ac852245db209f2de1a2
7luky <64417043+7luky@users.noreply.github.com>
2023-07-12 19:13:19 +0900

    #6.8 Form Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,26 +1,109 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setValue] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDoError("");
+//     setValue(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError("To do should be longer");
+//     }
+//     console.log("submit");
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           placeholder="Write a to do"
+//           type="text"
+//         />
+//         <button>Add</button>
+//         {toDoError !== "" ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
+
+// interface IFormData {
+//   [key: string]: string;
+// }
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName?: string;
+  username: string;
+  password: string;
+  password1: string;
+}
 
 function ToDoList() {
-  const [toDo, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+  const onValid = (data: any) => {
+    console.log(data);
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(errors?.email);
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write a to do"
-          type="text"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
         />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "write here" })}
+          placeholder="Email"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here", minLength: 10 })}
+          placeholder="FirstName"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="UserName"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "Password is required" })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "write here",
+            minLength: {
+              value: 5,
+              message: "Your password is too short",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
