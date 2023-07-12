3478c8a8ea91c270a2a454ed3390ad256e09b45f
7luky <64417043+7luky@users.noreply.github.com>
2023-07-12 23:36:29 +0900

    #6.18 Enums (12:35)

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,22 +1,25 @@
 import { atom, selector } from "recoil";
-import ToDo from "./ToDo";
 
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
-
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
