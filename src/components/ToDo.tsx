d7fa2d8116a22ad39170db14ae0d5b533b80da2f
7luky <64417043+7luky@users.noreply.github.com>
2023-07-12 23:08:11 +0900

    #6.15 Immutability part Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -3,10 +3,12 @@ import { IToDo, toDoState } from "./atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
+
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
       const newToDo = { text, id, category: name as any };
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,37 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "./atoms";
+import { toDoSelector, toDoState } from "./atoms";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
         {toDos.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
+      <hr />
+      <h1>Doing</h1>
+      <h2>To Do</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h1>Done</h1>
+      <h2>To Do</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
